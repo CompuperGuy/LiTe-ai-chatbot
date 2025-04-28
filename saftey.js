4a128263fd667182a7802738a2c77486b2fcbdc0
CompuperGuy <townsendcameliam@gmail.com>
2025-04-28 17:48:58 +0100

    Create saftey.js

---
+++ saftey.js
@@ -0,0 +1,4 @@
+const bannedWords = [
+  "nigga" , "nigger" , "fag" , "faggot" , "retard" , "spazy" , "kike" , "dagoe" , "negroe" , "gas the jews" , "fuck" , "shit" , "bitch" , "cock" , "cum" , "pussy" , "asshole" , "cunt" , "fucking" , "fucker" , "shiting" , "shitty" , "whore" , "dick" , "slut"
+];
+
